d9b9d940f9ab8b4a8ecbc105f9e083aa60227b8c
Rennanpaulino <rennanpsousa2003@gmail.com>
2025-09-02 21:39:30 -0300

    ajuste de confiança, prints salvando em cache, contagem de pessoas a cada 5 segundos, vídeo contínuo

--- Recog-modulo/recog.py
+++ Recog-modulo/recog.py
@@ -2,46 +2,40 @@
 from ultralytics import YOLO
 import cv2
 import matplotlib.pyplot as plt
-import shutil
 
 # abrir câmera (pode usar CAP_DSHOW para mais controle no Windows)
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FPS, 15)  # pedir 60fps
+cap.set(cv2.CAP_PROP_FPS, 60)  # alterar FPS
 
-print("FPS    :", cap.get(cv2.CAP_PROP_FPS))
+model = YOLO('yolov8l.pt')  #escolha do modelo,
+#podemos escolher M ou L pra ter mais precisão
+
+frame_cont = 0
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    cv2.imshow("Camera", frame)
+    
+    frame_cont += 1
+    if frame_cont % 300 == 0:  # Processa a cada 300 frames            
+    #predição e ajuste do arquivo
+        results = model(frame, classes=[0], conf=0.4, device="cpu")
+
+        # #Mostra da imagem
+        r = results[0]
+        annotated = r.plot()[:, :, ::-1]  # converte BGR -> RGB
+        plt.figure(figsize=(10,10))
+        plt.axis('off')
+
+        cv2.imshow("Pessoas", annotated) 
+    # Contas as pessoas detectadas
+        boxes = r.boxes
+        print("Número de pessoas detectadas:", boxes.shape[0])
+    else:
+        cv2.imshow("Pessoas", frame)
     if cv2.waitKey(1) & 0xFF == 27:  # ESC para sair
-        break
-
-#     # #Carregando o modelo
-    model = YOLO('yolov8n.pt')  #escolha do modelo,
-#podemos escolher M ou L pra ter mais precissão
-
-#predição e ajuste do arquivo
-    results = model.predict(
-        source=frame ,  # Usa o nome exato do arquivo
-        classes=[0],         # Apenas pessoas
-        conf=0.5,            # Confiança mínima
-        device="cpu",        # GPU ou CPU
-        save=True            # Salva imagem anotada
-    )
-
-    # #Mostra da imagem no Colab
-    r = results[0]               # resultado da primeira imagem
-    annotated = r.plot()[:, :, ::-1]  # converte BGR -> RGB
-    plt.figure(figsize=(10,10))
-    plt.imshow(annotated)
-    plt.axis('off')
-    plt.show()
-
-# Contas as pessoas detectadas
-    boxes = r.boxes
-    print("Número de pessoas detectadas:", boxes.shape[0])
+            break
 
 cap.release()
 cv2.destroyAllWindows()
